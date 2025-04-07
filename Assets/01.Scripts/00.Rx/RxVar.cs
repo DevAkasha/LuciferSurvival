5c2f71159a4e2a4f2e0b1a9fc3f21cf3436db558
DevAkasha <hmy9025@gmail.com>
2025-04-07 19:37:33 +0900

    [Setting] 두트윈, Rx 기초버전, Manager 원본 추가.
    [Setting] 두트윈, Rx 기초버전, Manager 원본 추가.

---
+++ Assets/01.Scripts/00.Rx/RxVar.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System;
+
+public class RxVar<T>
+{
+    private T _value;
+    private readonly List<Action<T>> _listeners = new();
+
+    public RxVar(T initialValue = default)
+    {
+        _value = initialValue;
+    }
+
+    public T Value => _value;
+
+    public void SetValue(T newValue)
+    {
+        if (!EqualityComparer<T>.Default.Equals(_value, newValue))
+        {
+            _value = newValue;
+            NotifyAll();
+        }
+    }
+
+    public void AddListener(Action<T> listener)
+    {
+        if (listener != null)
+        {
+            _listeners.Add(listener);
+            listener(_value); 
+        }
+    }
+
+    public void RemoveListener(Action<T> listener)
+    {
+        _listeners.Remove(listener);
+    }
+
+    private void NotifyAll()
+    {
+        foreach (var listener in _listeners)
+            listener(_value);
+    }
+}
