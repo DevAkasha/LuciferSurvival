a6271f1084b1c3c212b1f61e12d642bfb8c9099f
DevAkasha <hmy9025@gmail.com>
2025-04-08 10:20:21 +0900

    [add] TempEnvironment asset add
    [add] TempEnvironment asset add

---
+++ Assets/01.Scripts/02.Entity/CharacterModel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterModel: BaseModel
+{
+   public RxVar<float> Hp = new(100);
+}
---
+++ Assets/01.Scripts/02.Entity/Lucifer/LuciferEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuciferEntity : CharacterEntity
+{
+    public CharacterModel model;
+    
+    #region proxy property
+    public override float Hp 
+    { 
+        get => model.Hp.Value; 
+        set => model.Hp.SetValue(value);
+    }
+    #endregion
+
+    protected override void SetupModels()
+    {
+        model = new();
+    }
+
+
+
+}
