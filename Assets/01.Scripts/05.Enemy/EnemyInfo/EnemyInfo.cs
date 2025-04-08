5996f303e587bce2350a27e27872bcb51718a77c
YataGarasu8 <zmffldjdnld12@naver.com>
2025-04-08 17:57:45 +0900

    [add] 에너미 폴더 추가

---
+++ Assets/01.Scripts/05.Enemy/EnemyInfo/EnemyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyType
+{
+    Normal,
+    Elite,
+    Boss
+}
+public enum RaceType
+{
+    Angle
+}
+public enum JobType
+{
+    None
+}
+
+[CreateAssetMenu(fileName = "EnemyInfo", menuName = "ScriptableObjects/EnemyInfo")]
+public class EnemyInfo : ScriptableObject
+{
+    public string EnemyName = "전갱이";
+    public EnemyType enemyType;
+    public RaceType raceType;
+    public JobType jobType;
+    public float moveSpeed = 1f;
+}
