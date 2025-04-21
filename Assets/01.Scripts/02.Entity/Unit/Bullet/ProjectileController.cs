cb0e5de3622f56a50f3a6a83d368c98afbb3b30e
ProgramCnt <corfafa2@naver.com>
2025-04-21 10:27:21 +0900

    [fix] 투사체 움직임 수정

--- Assets/01.Scripts/02.Entity/Unit/Bullet/ProjectileController.cs
+++ Assets/01.Scripts/02.Entity/Unit/Bullet/ProjectileController.cs
@@ -14,10 +14,13 @@ public class ProjectileController : MonoBehaviour
     [SerializeField]   //나중에 빼기(컨트롤러 타깃 초기화 함수 만들 필요 있어보임)
     private Transform target;
 
+    private Vector3 direction;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = (target.position - transform.position).normalized;
+        direction.y = 0f;
     }
 
     void Update()
@@ -35,9 +38,6 @@ public class ProjectileController : MonoBehaviour
 
     private void ProjectileMove()
     {
-        Vector3 direction;
-        direction = (target.position - transform.position).normalized;
-        direction.y = 0f;
         transform.position += direction * projectile.speed * Time.deltaTime;
     }
 }
