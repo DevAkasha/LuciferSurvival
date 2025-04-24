ec8cbc84f35038d516c946db9248b8328acaa568
EunJi <euuu0eeu@gmail.com>
2025-04-24 11:15:33 +0900

    [wip] : 타일 코드 수정

--- Assets/01.Scripts/01.Manager/TileManager.cs
+++ Assets/01.Scripts/01.Manager/TileManager.cs
@@ -9,7 +9,7 @@ public class TileManager : Singleton<TileManager>
     public GameObject lockBlockTile; // 잠금타일
     public GameObject resourceTile; // 자원타일
 
-    int gridCount = 100;
+    int gridCount = 20;
 
     [SerializeField] private GameObject lockTileObj;
     [SerializeField] private Grid grid;
@@ -28,15 +28,28 @@ public class TileManager : Singleton<TileManager>
         {
             for (int y = 0; y < gridCount; y++)
             {
-                Vector2Int cell = new Vector2Int(x, y);
-                //if (excludedCells.Contains(cell))
-                //  continue; // 제외된 영역은 스킵
-                Vector3Int gridCell = new Vector3Int(x, y, 0);
-                Vector3 worldPos = grid.CellToWorld(gridCell);
+                Vector2Int cell = new Vector2Int(x, y); // 2D좌표로 만듬, 제외시킬 친구들
+                if (excludedCells.Contains(cell))
+                    continue; // 제외된 영역은 스킵
+                Vector3Int gridCell = new Vector3Int(x, y, 0); // grid 좌표로 변환
+                Vector3 worldPos = grid.CellToWorld(gridCell); // gridcell 위치를 월드좌표로 변환, 정확한 위치 파악
                 // XZ 평면 → Y를 0으로 고정
-                Vector3 adjustedPos = new Vector3(worldPos.x, 0f, worldPos.z);
-                Instantiate(lockTileObj, adjustedPos, Quaternion.identity, transform);
+                Vector3 addjustedPos = new Vector3(worldPos.x, 0f, worldPos.z); // y축은 0으로 고정, xz평면위에 배치, adjustedPos:최종위치값
+                Instantiate(lockTileObj, addjustedPos, Quaternion.identity, transform); // lockTile프리팹을 adjustedPos월드위치에 생성, 회전없이, 부모는 현재객체
             }
+        }
+    }
+
+    // 제외시킬 위치를 확인하기 위해 필요한 기즈모 (위치 시각화 용도)
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        foreach (var cell in excludedCells)
+        {
+            Vector3Int gridCell = new Vector3Int(cell.x, cell.y, 0);
+            Vector3 worldPos = grid.CellToWorld(gridCell);
+            Vector3 pos = new Vector3(worldPos.x, 0f, worldPos.z);
+            Gizmos.DrawWireCube(pos, Vector3.one * 2f);
         }
     }
 }
