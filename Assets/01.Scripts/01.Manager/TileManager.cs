2db19fd89dae025fa23809fe2ab09448e9c29c93
EunJi <euuu0eeu@gmail.com>
2025-04-24 18:05:08 +0900

    [feat] : 잠금타일 배치 및 자원타일 랜덤배치 기능 추가

--- Assets/01.Scripts/01.Manager/TileManager.cs
+++ Assets/01.Scripts/01.Manager/TileManager.cs
@@ -1,19 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TileManager : Singleton<TileManager>
 {
     protected override bool IsPersistent => false;
 
-    public GameObject lockBlockTile; // 잠금타일
-    public GameObject resourceTile; // 자원타일
+    int gridCount = 20;     // 전체 크기
+    int resourceCount = 20; // 자원 갯수
 
-    int gridCount = 20;
-
-    [SerializeField] private GameObject lockTileObj;
+    [SerializeField] private GameObject lockTileObj;            // 잠금타일 프리펩
+    [SerializeField] private List<GameObject> resourceTileObj;  // 자원타일 프리펩 리스트
     [SerializeField] private Grid grid;
-    [SerializeField] private List<Vector2Int> excludedCells; // 제외할 셀 좌표들
+    [SerializeField] private List<Vector2Int> excludedCells;    // 제외할 셀 좌표 리스트
 
     private void Start()
     {
@@ -21,42 +21,67 @@ public class TileManager : Singleton<TileManager>
         SetLockTileMap();
     }
 
+    /// <summary>
+    /// 자원타일 배치
+    /// </summary>
     public void SetResourceTileMap()
     {
+        // 자원타일이 이미 배치된 좌표 확인용
+        List<Vector2Int> useTileObj = new List<Vector2Int>();
 
+        for (int i = 0; i < resourceCount; i++)
+        {
+            int randomX = Random.Range(0, gridCount);
+            int randomY = Random.Range(0, gridCount);
+            int resourceType = Random.Range(0, resourceTileObj.Count);
+
+            Vector2Int cell = new Vector2Int(randomX, randomY); // 랜덤 좌표 생성
+            if (useTileObj.Contains(cell)) // 이미 배치된 좌표일경우 반복횟수 복원
+            {
+                i--;
+                continue; 
+            } 
+            useTileObj.Add(cell); // 좌표 등록
+
+            Vector3Int gridCell = new Vector3Int(randomX, randomY, 0); // grid 좌표로 변환
+            Vector3 worldPos = grid.CellToWorld(gridCell); // 월드좌표로 변환
+            Vector3 addjustedPos = new Vector3(worldPos.x, 0f, worldPos.z); // 최종위치값 = y축은 0으로 고정, xz평면위에 배치
+            Instantiate(resourceTileObj[resourceType], addjustedPos, Quaternion.identity, transform); // 프리팹들을 adjustedPos월드위치에 생성
+        }
     }
 
-    // 잠금타일 배치 메서드
+    /// <summary>
+    /// 잠금타일
+    /// </summary>
     public void SetLockTileMap()
     {
-        // 프리펩 크기 조절할수있게 추가 하나 해줘보기, 공부하기
-        // 현재 포지션 000기준으로 생성되는 형식인데 중앙에서 퍼져나가는 형식으로도 가능한지 나중에 확인해보기
-
         for (int x = 0; x < gridCount; x++)
         {
             for (int y = 0; y < gridCount; y++)
             {
-                Vector2Int cell = new Vector2Int(x, y); // 2D좌표로 만듬, 제외시킬 친구들
+                Vector2Int cell = new Vector2Int(x, y); // 제외할 타일 목록
                 if (excludedCells.Contains(cell))
                     continue; // 제외된 영역은 스킵
-                Vector3Int gridCell = new Vector3Int(x, y, 0); // grid 좌표로 변환
-                Vector3 worldPos = grid.CellToWorld(gridCell); // gridcell 위치를 월드좌표로 변환, 정확한 위치 파악, XZ 평면 → Y를 0으로 고정
-                Vector3 addjustedPos = new Vector3(worldPos.x, 0f, worldPos.z); // y축은 0으로 고정, xz평면위에 배치, adjustedPos:최종위치값
-                Instantiate(lockTileObj, addjustedPos, Quaternion.identity, transform); // lockTile프리팹을 adjustedPos월드위치에 생성, 회전없이, 부모는 현재객체
+                Vector3Int gridCell = new Vector3Int(x, y, 0);
+                Vector3 worldPos = grid.CellToWorld(gridCell);
+                Vector3 addjustedPos = new Vector3(worldPos.x, 0f, worldPos.z);
+                Instantiate(lockTileObj, addjustedPos, Quaternion.identity, transform); // 프리팹을 adjustedPos월드위치에 생성
             }
         }
     }
 
-    // 잠금타일 제외시킬 위치를 확인하기 위해 필요한 기즈모 (위치 시각화 용도)
+    /// <summary>
+    /// 자금타일 위치확인용 기즈모
+    /// </summary>
     private void OnDrawGizmosSelected()
     {
-        Gizmos.color = Color.red;
+        Gizmos.color = Color.red; // 기즈모 설정 색
         foreach (var cell in excludedCells)
         {
-            Vector3Int gridCell = new Vector3Int(cell.x, cell.y, 0);
-            Vector3 worldPos = grid.CellToWorld(gridCell);
-            Vector3 pos = new Vector3(worldPos.x, 0f, worldPos.z);
-            Gizmos.DrawWireCube(pos, Vector3.one * 2f);
+            Vector3Int gridCell = new Vector3Int(cell.x, cell.y, 0); // 3D좌표로 변환
+            Vector3 worldPos = grid.CellToWorld(gridCell); // 월드좌표로 변환
+            Vector3 pos = new Vector3(worldPos.x, 0f, worldPos.z); // 위치
+            Gizmos.DrawWireCube(pos, Vector3.one * 2f); // 와이어프레임 박스 표시 (222 크기의 정육면체)
         }
     }
 }
