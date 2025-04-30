fc0e4a53c02b25313648aab61177c1e89e26545c
DevAkasha <hmy9025@gmail.com>
2025-04-30 14:46:46 +0900

    [feat] 리팩토링 임시커밋

--- Assets/01.Scripts/00.Origin/PowerModel/Base/BaseModel.cs
+++ Assets/01.Scripts/00.Origin/PowerModel/Base/BaseModel.cs
@@ -1,15 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-public interface ITrackableRxModel
-{
-    void RegisterRx(RxBase rx); // Rx 필드를 모델에 등록
-}
 
-public interface IModifiableTarget
-{
-    IEnumerable<IModifiable> GetModifiables(); // 수정 가능한 필드 목록 반환
-}
 
 public abstract class BaseModel : IModifiableTarget, ITrackableRxModel
 {
--- Assets/01.Scripts/00.Origin/PowerModel/Common/Interface.cs
+++ Assets/01.Scripts/00.Origin/PowerModel/Common/Interface.cs
@@ -1,2 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+
+public interface ITrackableRxModel
+{
+    void RegisterRx(RxBase rx); // Rx 필드를 모델에 등록
+}
+
+public interface IModifiableTarget
+{
+    IEnumerable<IModifiable> GetModifiables(); // 수정 가능한 필드 목록 반환
+}
+
