f6c7f01e6c18e68d33eec4d212f4601cd33d17f3
YataGarasu8 <zmffldjdnld12@naver.com>
2025-05-07 17:36:14 +0900

    [feat] WaveDataSO 작성

---
+++ Assets/01.Scripts/ScriptableObject/WaveDataSO.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveDataSO : BaseDataSO
+{
+    public int Stage;
+
+    public string enemy1rcode;
+    public int enemy1Count;
+    public float enemy1Sec;
+
+    public string enemy2rcode;
+    public int enemy2Count;
+    public float enemy2Sec;
+
+    public string enemy3rcode;
+    public int enemy3Count;
+    public float enemy3Sec;
+
+    public string enemy4rcode;
+    public int enemy4Count;
+    public float enemy4Sec;
+}
