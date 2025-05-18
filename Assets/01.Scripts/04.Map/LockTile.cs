ed7f24606f1830305c2bff1bb6234c14a790da9f
EunJi <euuu0eeu@gmail.com>
2025-05-19 03:21:54 +0900

    [wip] 영혼석 획득 및 사용 테스트

--- Assets/01.Scripts/04.Map/LockTile.cs
+++ Assets/01.Scripts/04.Map/LockTile.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 // 프리펩에 붙일 스크립트
 public class LockTile : BaseInteractable
 {
+    [SerializeField] private int unlockCost = 5; // 기본값 5로 설정, Inspector에서 조정 가능
+
     public override void Interact(PlayerEntity player)
     {
-        RemoveTile();
+        if (StageManager.Instance.UseSoulStone(unlockCost))
+        {
+            // 재화가 충분하면 타일 제거
+            RemoveTile();
+        }
+        else
+        {
+            Debug.Log("영혼석 부족");
+        }
     }
 
     // 파괴
