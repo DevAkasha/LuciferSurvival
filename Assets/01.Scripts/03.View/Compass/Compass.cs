6931613ed5e247623a4678d6c3645acac80665d2
DevAkasha <hmy9025@gmail.com>
2025-05-19 23:51:58 +0900

    [feat] Compass 기능 완료
    [feat] Compass 기능 완료

--- Assets/01.Scripts/03.View/Compass/Compass.cs
+++ Assets/01.Scripts/03.View/Compass/Compass.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Compass : MonoBehaviour
+public class CompassArrow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform playerTransform; // 플레이어 위치
+    [SerializeField] private Transform targetTransform; // 추적할 타겟
+    [SerializeField] private RectTransform arrowImage; // 화살표 이미지
+
+    private void Update()
     {
-        
+        if (playerTransform == null || targetTransform == null || arrowImage == null)
+            return;
+
+        UpdateCompassArrow();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateCompassArrow()
     {
-        
+        // 플레이어에서 타겟으로의 방향 벡터 계산 (y축 무시)
+        Vector3 targetDirection = targetTransform.position - playerTransform.position;
+        targetDirection.y = 0f;
+
+        // 타겟 방향을 각도로 변환
+        float angle = Mathf.Atan2(targetDirection.x, targetDirection.z) * Mathf.Rad2Deg;
+
+        // 화살표 이미지 회전
+        arrowImage.localRotation = Quaternion.Euler(0, 0, -angle);
+    }
+
+    // 타겟 변경 메서드
+    public void SetTarget(Transform newTarget)
+    {
+        targetTransform = newTarget;
+    }
+
+    // 플레이어 변경 메서드
+    public void SetPlayer(Transform newPlayer)
+    {
+        playerTransform = newPlayer;
     }
 }
