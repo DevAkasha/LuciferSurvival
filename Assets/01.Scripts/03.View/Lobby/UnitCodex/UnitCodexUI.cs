1bc2f8f7f8e8f8a51aa73945448a9505d617e0e3
ProgramCnt <corfafa2@naver.com>
2025-05-20 14:33:13 +0900

    [feat] 도감 상세보기 작업 완료

--- Assets/01.Scripts/03.View/Lobby/UnitCodex/UnitCodexUI.cs
+++ Assets/01.Scripts/03.View/Lobby/UnitCodex/UnitCodexUI.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class UnitCodexUI : UIBase
 {
     [SerializeField]
     private UnitCard cardPrefab;
+
+    [SerializeField]
+    private Transform contentTransform;
 
     public override void HideDirect()
     {
@@ -14,11 +18,15 @@ public class UnitCodexUI : UIBase
 
     public override void Opened(object[] param)
     {
-        List<UnitDataSO> units = DataManager.Instance.GetDatas<UnitDataSO>();
+        List<UnitDataSO> units = DataManager.Instance.GetDatas<UnitDataSO>()
+        .OrderByDescending(m => m.grade)   // 높은 등급 우선
+        .ToList();
 
         foreach (UnitDataSO data in units)
         {
-
+            UnitCard card = Instantiate(cardPrefab, contentTransform);
+            card.SetUnitCard(data);
+            card.SetDetailTransform(transform.parent);
         }
     }
 }
