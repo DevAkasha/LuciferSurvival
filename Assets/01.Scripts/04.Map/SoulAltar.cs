53435f1d7908f88a415c783c3045dfe7b8281d5d
EunJi <euuu0eeu@gmail.com>
2025-05-22 10:21:43 +0900

    [add] 영혼의 제단 추가중

---
+++ Assets/01.Scripts/04.Map/SoulAltar.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoulAltar : BaseInteractable
+{
+    public override void Interact(PlayerEntity player)
+    {
+        // UI뜨게 해야함
+    }
+}
