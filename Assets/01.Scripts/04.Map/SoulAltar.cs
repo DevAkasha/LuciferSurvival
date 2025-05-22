d5f5b487bf55d91c0d06bcf958094ed4985d65be
EunJi <euuu0eeu@gmail.com>
2025-05-22 17:24:58 +0900

    [add] 영혼의 제단 상호작용 기능 추가

--- Assets/01.Scripts/04.Map/SoulAltar.cs
+++ Assets/01.Scripts/04.Map/SoulAltar.cs
@@ -6,21 +6,15 @@ public class SoulAltar : BaseInteractable
 {
     [SerializeField] private UnitManageUI manageUI;
     [SerializeField] private Transform canvas;
-    [Header("나침반")]
-    [SerializeField] private CompassArrow compassArrow;
     
     public override void Interact(PlayerEntity player)
     {
         OpenSummonUI();
     }
 
-    private void Start()
-    {
-        compassArrow.SetTarget(this.gameObject.transform);
-    }
-
     public void OpenSummonUI()
-    {
+    { 
+        canvas = StageUIManager.Instance.GetCanvasTransform();
         UIManager.Show(manageUI, canvas);
     }
 }
