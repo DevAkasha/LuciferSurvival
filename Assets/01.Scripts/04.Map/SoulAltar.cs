6d79b001a48229d79eff94b3392a5a51609b4f25
EunJi <euuu0eeu@gmail.com>
2025-05-22 14:35:43 +0900

    [fix] 파밍 데이터 수정

--- Assets/01.Scripts/04.Map/SoulAltar.cs
+++ Assets/01.Scripts/04.Map/SoulAltar.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class SoulAltar : BaseInteractable
 {
+    [SerializeField] private UnitManageUI manageUI;
+
+    [Header("나침반")]
+    [SerializeField] private CompassArrow compassArrow;
+    
     public override void Interact(PlayerEntity player)
     {
-        // UI뜨게 해야함
+        OpenSummonUI();
+    }
+
+    private void Start()
+    {
+        if (compassArrow != null)
+        {
+            compassArrow.SetTarget(this.transform);
+        }
+    }
+
+    public void OpenSummonUI()
+    {
+        UIManager.Show(manageUI, transform.parent);
     }
 }
