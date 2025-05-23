94b8246ae6e4ed502f9facf8324a937723d83877
EunJi <euuu0eeu@gmail.com>
2025-05-23 12:19:42 +0900

    [wip] 맵 및 잠금타일 수정

--- Assets/01.Scripts/04.Map/LockTile.cs
+++ Assets/01.Scripts/04.Map/LockTile.cs
@@ -5,20 +5,18 @@ using UnityEngine;
 // 프리펩에 붙일 스크립트
 public class LockTile : BaseInteractable
 {
-    [SerializeField] private int unlockCost = 5; // 기본값 5로 설정, Cost부분은 나중에 수정 혹은 삭제예정
+    [SerializeField] private int unlockCost = 10;
 
     public override void Interact(PlayerEntity player)
     {
-        // 영혼석이 아닌 영혼핵으로 변경하기 - 몬스터 처치 시 획득하는 재화
-
-        //if (StageManager.Instance.UseSoulStone(unlockCost))
-        //{
-        //    RemoveTile();
-        //}
-        //else
-        //{
-        //    Debug.Log("영혼석 부족");
-        //}
+        if (StageManager.Instance.ReduceSoulCore(unlockCost))
+        {
+            RemoveTile(); 
+        }
+        else
+        {
+            // UI 혹은 불가 시 동작할거
+        }
     }
 
     // 파괴
